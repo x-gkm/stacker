1c0db69b0e351db35bd189be5bc7448ef44b5a28
Gökmen Kaplan <48455693+x-gkm@users.noreply.github.com>
2026-03-30 10:22:57 +0300

    Add a main menu

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::time::Instant;
 
 use async_trait::async_trait;
+use egui_macroquad::egui;
 use macroquad::prelude::*;
 use stacker_engine::{
     Action, Cell, Config, Coords, Direction, Engine, GRID_HEIGHT, HoldPiece, Input, PILE_WIDTH,
@@ -192,9 +193,35 @@ impl Scene for Game {
     }
 }
 
+struct MainMenu;
+
+#[async_trait]
+impl Scene for MainMenu {
+    async fn execute(&mut self) -> Box<dyn Scene> {
+        loop {
+
+            let mut clicked = false;
+
+            egui_macroquad::ui(|ctx| {
+                egui::CentralPanel::default().show(ctx, |ui| {
+                    clicked = ui.button("play").clicked();
+                });
+            });
+
+            if clicked {
+                return Box::new(Game::default());
+            }
+
+            egui_macroquad::draw();
+
+            next_frame().await;
+        }
+    }
+}
+
 #[macroquad::main("stacker")]
 async fn main() {
-    let mut scene: Box<dyn Scene> = Box::new(Game::default());
+    let mut scene: Box<dyn Scene> = Box::new(MainMenu);
     loop {
         scene = scene.execute().await;
     }
